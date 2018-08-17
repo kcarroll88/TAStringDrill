60aa0953577559b9fcd43fa6424a2c7b56cba6dd
Keenan Carroll <keenan.carroll.va@gmail.com>
2018-08-17 12:51:49 -0400

    simple string tests
    First string builder

--- TAStringDrill/Program.cs
+++ TAStringDrill/Program.cs
@@ -10,6 +10,19 @@ namespace TAStringDrill
     {
         static void Main(string[] args)
         {
+            string fName = "Keenan";
+            string mName = "Andre";
+            string lName = "Carroll";
+            Console.WriteLine(fName + " " + mName + " " + lName);
+            fName = fName.ToUpper();
+            Console.WriteLine(fName);
+
+            StringBuilder pg = new StringBuilder("This is my first string builder.");
+            pg.Append("Now I can continue to add to my string without loosing performance.");
+            pg.Append("This is my last sentence.");
+            pg.Append("Ok one more, Have a good day!");
+            Console.WriteLine(pg);
+            Console.Read();
         }
     }
 }
